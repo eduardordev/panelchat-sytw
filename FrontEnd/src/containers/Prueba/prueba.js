2ac9daa98ea44d7978652e2450644768a8f38cf8
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2021-05-05 18:01:07 -0600

    Contenido estatico

--- FrontEnd/src/containers/Prueba/prueba.js
+++ FrontEnd/src/containers/Prueba/prueba.js
@@ -1,12 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+import './grid.css';
 
 function prueba() {
+    const elements = [Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios()];
     return (
-        <div>
-            <h1>Esto es un componente</h1>
 
-        </div>
+        <ul>
+            {elements.map((value, index) => {
+                return <div key={index}>{value}</div>
+            })}
+        </ul>
+
+
     )
 }
+
+function Usuarios() {
+    return (
+        <Container fluid className="grid">
+            <Row justify="between">
+                <Col className="Usuarios">
+
+                    <h1>User Name</h1>
+                    <p> CHAT</p>
+
+
+                </Col>
+                <Col className="botones">
+
+                    <button className="btnEstado">
+                        START
+                    </button>
+                    <button className="btnIniciar">
+                        COMPLETED
+                    </button>
+                    <button className="btnFail">
+                        FAILED
+                    </button>
+                    <button className="btnHistory">
+                        HISTORY
+                    </button>
+
+                </Col>
+
+            </Row>
+
+
+
+        </Container>
+
+    )
+
+}
+
 
 export default prueba;
